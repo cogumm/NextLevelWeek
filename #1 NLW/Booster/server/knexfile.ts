9e1d28745dbac1717b661f429743ddd34322f27e
Gabriel F. Vilar <cogumm@gmail.com>
2020-06-02 16:40:43 -0300

    :sparkles: - Booster - Server - Criando o seeds

--- #1 NLW/Booster/server/knexfile.ts
+++ #1 NLW/Booster/server/knexfile.ts
@@ -18,6 +18,9 @@ module.exports = {
         migrations: {
             directory: path.resolve(__dirname, "src", "database", "migrations"),
         },
+        seeds: {
+            directory: path.resolve(__dirname, "src", "database", "seeds"),
+        },
         useNullAsDefault: true,
     },
 
---
+++ #1 NLW/Booster/server/src/database/seeds/create_items.ts
@@ -0,0 +1,30 @@
+import Knex from "knex";
+
+export async function seed(knex: Knex) {
+    await knex("items").insert([
+        {
+            title: "Lâmapdas",
+            image: "lampadas.svg",
+        },
+        {
+            title: "Pilhas e Baterias",
+            image: "beterias.svg",
+        },
+        {
+            title: "Papéis e Papelão",
+            image: "lampadas.svg",
+        },
+        {
+            title: "Resíduos Eletrônicos",
+            image: "eletronicos.svg",
+        },
+        {
+            title: "Resíduos Orgânicos",
+            image: "organicos.svg",
+        },
+        {
+            title: "Óleo de Cozinha",
+            image: "oleo.svg",
+        },
+    ]);
+}
