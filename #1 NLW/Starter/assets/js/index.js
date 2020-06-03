8ed43fa4f1b997fb7baaf7b60648f9001986ec99
Gabriel F. Vilar <cogumm@gmail.com>
2020-06-03 14:08:41 -0300

    :sparkles: - Starter - Modal de pesquisa index

---
+++ #1 NLW/Starter/assets/js/index.js
@@ -0,0 +1,15 @@
+/**
+ * Adicionar / remover a classe "hide".
+ */
+
+const buttonSearch = document.querySelector("#page-home main a");
+const modal = document.querySelector("#modal");
+const close = document.querySelector("#modal .header a");
+
+buttonSearch.addEventListener("click", () => {
+    modal.classList.remove("hide");
+});
+
+close.addEventListener("click", () => {
+    modal.classList.add("hide");
+});
