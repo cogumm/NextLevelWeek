ebb9a4b13db78fa781277c5453bf6f461f98d5a5
Gabriel F. Vilar <cogumm@gmail.com>
2020-06-04 17:22:53 -0300

    :recycle: - Booster Web - Buscando estados e cidades da API do IBGE

--- #1 NLW/Booster/web/src/pages/CreatePoint/index.tsx
+++ #1 NLW/Booster/web/src/pages/CreatePoint/index.tsx
@@ -1,9 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, ChangeEvent } from "react";
 import { Link } from "react-router-dom";
-
 import { FiArrowLeft } from "react-icons/fi";
-
 import { Map, TileLayer, Marker } from "react-leaflet";
+import axios from "axios";
 
 import api from "../../services/api";
 
@@ -21,18 +20,92 @@ interface Item {
     image_url: string;
 }
 
-const CreatePoint = () => {
-    /**
-     * Evitar que toda vez que o componente mude, chame a API.
-     */
-    const [items, setItems] = useState<Item[]>([]);
+interface IBGEUF {
+    sigla: string;
+}
 
+interface IBGECity {
+    nome: string;
+}
+
+const CreatePoint = () => {
+    const [items, setItems] = useState<Item[]>([]);
+    const [ufs, setUfs] = useState<string[]>([]);
+    const [cities, setCities] = useState<string[]>([]);
+
+    const [selectedUf, setCity] = useState("0");
+    const [selectedCity, setSelectedCity] = useState("0");
+
+    /**
+     * Evitar que toda vez que o componente mude, chame a API local.
+     */
     useEffect(() => {
         api.get("/items").then((res) => {
             // console.log(res);
             setItems(res.data);
         });
     }, []);
+
+    /**
+     * Chamanda da API do IBGE para UFs.
+     */
+    useEffect(() => {
+        axios
+            .get<IBGEUF[]>(
+                "https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome"
+            )
+            .then((res) => {
+                // console.log(res);
+                const ufInitials = res.data.map((uf) => uf.sigla);
+
+                // console.log(ufInitials);
+                setUfs(ufInitials);
+            });
+    }, []);
+
+    /**
+     * Chamada da API do IBGE por municípios.
+     */
+    useEffect(() => {
+        // Carregar as cidades sempre que alterar as UF.
+        // console.log("Funfou", selectedUf);
+
+        if (selectedUf === "0") {
+            return;
+        }
+
+        axios
+            .get<IBGECity[]>(
+                `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios`
+            )
+            .then((res) => {
+                // console.log(res);
+                const cityName = res.data.map((city) => city.nome);
+
+                setCities(cityName);
+            });
+    }, [selectedUf]);
+
+    /**
+     * Toda vez que o usuário altera a UF chama esta função.
+     */
+    function handleSelectUf(e: ChangeEvent<HTMLSelectElement>) {
+        // console.log(e.target.value);
+        const uf = e.target.value;
+
+        setCity(uf);
+    }
+
+    /**
+     * Armazenando a cidade
+     */
+    function handleSelectCity(e: ChangeEvent<HTMLSelectElement>) {
+        const city = e.target.value;
+
+        // console.log(city);
+
+        setSelectedCity(city);
+    }
 
     return (
         <div id="page-create-point">
@@ -90,15 +163,35 @@ const CreatePoint = () => {
                     <div className="field-group">
                         <div className="field">
                             <label htmlFor="uf">Estado</label>
-                            <select name="uf" id="uf">
-                                <option value="0">Selecione uma UF</option>
+                            <select
+                                name="uf"
+                                id="uf"
+                                value={selectedUf}
+                                onChange={handleSelectUf}
+                            >
+                                <option value="0">Selecione um estado</option>
+                                {ufs.map((uf) => (
+                                    <option key={uf} value={uf}>
+                                        {uf}
+                                    </option>
+                                ))}
                             </select>
                         </div>
 
                         <div className="field">
                             <label htmlFor="city">Cidade</label>
-                            <select name="city" id="city">
+                            <select
+                                name="city"
+                                id="city"
+                                value={selectedCity}
+                                onChange={handleSelectCity}
+                            >
                                 <option value="0">Selecione uma cidade</option>
+                                {cities.map((city) => (
+                                    <option key={city} value={city}>
+                                        {city}
+                                    </option>
+                                ))}
                             </select>
                         </div>
                     </div>
