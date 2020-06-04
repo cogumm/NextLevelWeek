f04197322e052366f965155c6a81e8e04fa6cce9
Gabriel F. Vilar <cogumm@gmail.com>
2020-06-04 13:06:12 -0300

    :recycle: - Starter - Template Nunjucks e alguns ajustes.

--- #1 NLW/Starter/src/server.js
+++ #1 NLW/Starter/src/server.js
@@ -4,8 +4,47 @@ const express = require("express");
 const server = express();
 
 /**
+ * Configurando o Nunjucks = template engine
+ */
+const nunjucks = require("nunjucks");
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true,
+});
+
+/**
+ * Rota pública para ser acessada de forma direta.
+ */
+server.use(express.static("public"));
+
+/**
+ * Enviando o arquivo index.html
+ */
+server.get("/", (req, res) => {
+    return res.render("index.html");
+});
+
+/**
+ * Enviando o arquivo create-point.html
+ */
+server.get("/create", (req, res) => {
+    return res.render("create-point.html", {
+        title: "Ecoleta - Criar ponto de coleta",
+    });
+});
+
+/**
+ * Enviando o arquivo search.html
+ */
+server.get("/search", (req, res) => {
+    return res.render("search.html", {
+        title: "Ecoleta - Resultado da pesquisa",
+    });
+});
+
+/**
  * Rodando o servidor back-end.
  */
-server.listen(3000, () => {
-    console.log("Servidor backend inicializado com sucesso na porta " + 3000);
+server.listen(3001, () => {
+    console.log("Servidor backend inicializado com sucesso na porta " + 3001);
 });
