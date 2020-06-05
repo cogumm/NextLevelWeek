b4d2b2c00d77428bd816714549b5e79ff952809e
Gabriel F. Vilar <cogumm@gmail.com>
2020-06-05 13:29:40 -0300

    :sparkles: - Booster Mobile - Tela de detalhes

--- #1 NLW/Booster/mobile/src/pages/Detail/index.tsx
+++ #1 NLW/Booster/mobile/src/pages/Detail/index.tsx
@@ -1,12 +1,66 @@
 import React from "react";
-import { View } from "react-native";
+import {
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  Text,
+  SafeAreaView,
+} from "react-native";
+import { Feather as Icon, FontAwesome } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+import { RectButton } from "react-native-gesture-handler";
 
 const Detail: React.FC = () => {
-  return <View />;
+  const navigation = useNavigation();
+
+  /**
+   * Função para voltar na página anterior.
+   */
+  function handleBackToTheFuture() {
+    navigation.goBack();
+  }
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={handleBackToTheFuture}>
+          <Icon name="arrow-left" size={20} color="#34cb79" />
+        </TouchableOpacity>
+
+        <Image
+          style={styles.pointImage}
+          source={{
+            uri:
+              "https://roamthegnome.com/wp-content/uploads/2019/06/japanese-supermarkets-in-japan-header.jpg",
+          }}
+        />
+
+        <Text style={styles.pointName}>Mercadão</Text>
+        <Text style={styles.pointItems}>Lâmpadas, Óleo de cozinha</Text>
+
+        <View style={styles.address}>
+          <Text style={styles.addressTitle}>Endereço</Text>
+          <Text style={styles.addressContent}>Rual aaaaa</Text>
+        </View>
+      </View>
+
+      <View style={styles.footer}>
+        <RectButton style={styles.button} onPress={() => {}}>
+          <FontAwesome name="whatsapp" size={20} color="#FFF" />
+          <Text style={styles.buttonText}>WhatsApp</Text>
+        </RectButton>
+
+        <RectButton style={styles.button} onPress={() => {}}>
+          <Icon name="mail" size={20} color="#FFF" />
+          <Text style={styles.buttonText}>E-mail</Text>
+        </RectButton>
+      </View>
+    </SafeAreaView>
+  );
 };
 
-export default Detail;
-/* const styles = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 32,
@@ -14,68 +68,70 @@ export default Detail;
   },
 
   pointImage: {
-    width: '100%',
+    width: "100%",
     height: 120,
-    resizeMode: 'cover',
+    resizeMode: "cover",
     borderRadius: 10,
     marginTop: 32,
   },
 
   pointName: {
-    color: '#322153',
+    color: "#322153",
     fontSize: 28,
-    fontFamily: 'Ubuntu_700Bold',
+    fontFamily: "Ubuntu_700Bold",
     marginTop: 24,
   },
 
   pointItems: {
-    fontFamily: 'Roboto_400Regular',
+    fontFamily: "Roboto_400Regular",
     fontSize: 16,
     lineHeight: 24,
     marginTop: 8,
-    color: '#6C6C80'
+    color: "#6C6C80",
   },
 
   address: {
     marginTop: 32,
   },
-  
+
   addressTitle: {
-    color: '#322153',
-    fontFamily: 'Roboto_500Medium',
+    color: "#322153",
+    fontFamily: "Roboto_500Medium",
     fontSize: 16,
   },
 
   addressContent: {
-    fontFamily: 'Roboto_400Regular',
+    fontFamily: "Roboto_400Regular",
     lineHeight: 24,
     marginTop: 8,
-    color: '#6C6C80'
+    color: "#6C6C80",
   },
 
   footer: {
     borderTopWidth: StyleSheet.hairlineWidth,
-    borderColor: '#999',
+    borderColor: "#999",
     paddingVertical: 20,
     paddingHorizontal: 32,
-    flexDirection: 'row',
-    justifyContent: 'space-between'
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
-  
+
   button: {
-    width: '48%',
-    backgroundColor: '#34CB79',
+    width: "48%",
+    backgroundColor: "#34CB79",
     borderRadius: 10,
     height: 50,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center'
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
   },
 
   buttonText: {
     marginLeft: 8,
-    color: '#FFF',
+    color: "#FFF",
     fontSize: 16,
-    fontFamily: 'Roboto_500Medium',
+    fontFamily: "Roboto_500Medium",
   },
-}); */
+});
+
+export default Detail;
