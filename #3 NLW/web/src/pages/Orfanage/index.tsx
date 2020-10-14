31a6798bba14feaaba880e87c35366f23a579249
Gabriel F. Vilar <gabriel@cogumm.net>
2020-10-14 18:32:13 -0300

    #3 NLW Web - Página de detalhe do orfanato

---
+++ #3 NLW/web/src/pages/Orfanage/index.tsx
@@ -0,0 +1,124 @@
+import React from "react";
+import { FaWhatsapp } from "react-icons/fa";
+import { FiClock, FiInfo } from "react-icons/fi";
+import { Map, Marker, TileLayer } from "react-leaflet";
+import { Link, useHistory } from 'react-router-dom';
+
+import Leaflet from 'leaflet';
+
+import mapMarkerImg from '../../images/map-marker.svg';
+
+import {
+    Container,
+    Content,
+    DetailsContainer,
+    ImagesContainer,
+    ButtonImage,
+    OrphanageDetails,
+    MapContainer,
+    OpenDetails
+} from './styles';
+
+const happyMapIcon = Leaflet.icon({
+    iconUrl: mapMarkerImg,
+
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [0, -60]
+});
+
+const Orphanage: React.FC = () => {
+    const { goBack } = useHistory();
+
+    return (
+        <Container>
+            {/* <SideBar /> */}
+            {/* <aside>
+                <img src={mapMarkerImg} alt="Happy" />
+
+                <footer>
+                    <button type="button" onClick={goBack}>
+                        <FiArrowLeft size={24} color="#FFF" />
+                    </button>
+                </footer>
+            </aside> */}
+            <Content>
+                <DetailsContainer>
+                    <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das meninas" />
+
+                    <ImagesContainer>
+                        <ButtonImage active type="button">
+                            <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das meninas" />
+                        </ButtonImage>
+                        <ButtonImage type="button">
+                            <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das meninas" />
+                        </ButtonImage>
+                        <ButtonImage type="button">
+                            <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das meninas" />
+                        </ButtonImage>
+                        <ButtonImage type="button">
+                            <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das meninas" />
+                        </ButtonImage>
+                        <ButtonImage type="button">
+                            <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das meninas" />
+                        </ButtonImage>
+                        <ButtonImage type="button">
+                            <img src="https://www.gcd.com.br/wp-content/uploads/2020/08/safe_image.jpg" alt="Lar das meninas" />
+                        </ButtonImage>
+                    </ImagesContainer>
+
+                    <OrphanageDetails>
+                        <h1>Lar das meninas</h1>
+                        <p>Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.</p>
+
+                        <MapContainer>
+                            <Map
+                                center={[-27.2092052, -49.6401092]}
+                                zoom={16}
+                                style={{ width: '100%', height: 280 }}
+                                dragging={false}
+                                touchZoom={false}
+                                zoomControl={false}
+                                scrollWheelZoom={false}
+                                doubleClickZoom={false}
+                            >
+                                <TileLayer
+                                    url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+                                />
+                                <Marker interactive={false} icon={happyMapIcon} position={[-27.2092052, -49.6401092]} />
+                            </Map>
+
+                            <footer>
+                                <Link to="/">Ver rotas no Google Maps</Link>
+                            </footer>
+                        </MapContainer>
+
+                        <hr />
+
+                        <h2>Instruções para visita</h2>
+                        <p>Venha como se sentir mais à vontade e traga muito amor para dar.</p>
+
+                        <OpenDetails>
+                            <div>
+                                <FiClock size={32} color="#15B6D6" />
+                                Segunda à Sexta <br />
+                                8h às 18h
+                            </div>
+                            <div>
+                                <FiInfo size={32} color="#39CC83" />
+                                Atendemos <br />
+                                fim de semana
+                            </div>
+                            <button type="button" className="contact-button">
+                                <FaWhatsapp size={20} color="#FFF" />
+                                Entrar em contato
+                            </button>
+                        </OpenDetails>
+                    </OrphanageDetails>
+                </DetailsContainer>
+            </Content>
+        </Container>
+    );
+}
+
+export default Orphanage;
---
+++ #3 NLW/web/src/pages/Orfanage/styles.ts
@@ -0,0 +1,205 @@
+import styled, { css } from 'styled-components'
+
+interface IButtonImageProps {
+    active?: boolean
+}
+
+export const Container = styled.div`
+    display: flex;
+    min-height: 100vh;
+`
+
+export const Content = styled.main`
+    flex: 1;
+`
+
+export const DetailsContainer = styled.div`
+    width: 700px;
+    margin: 64px auto;
+    background: #FFFFFF;
+    border: 1px solid #D3E2E5;
+    border-radius: 20px;
+    overflow: hidden;
+
+    > img {
+        width: 100%;
+        height: 300px;
+        object-fit: cover;
+    }
+`
+
+export const ImagesContainer = styled.div`
+    display: grid;
+    grid-template-columns: repeat(6, 1fr);
+    column-gap: 16px;
+    margin: 16px 40px 0;
+`
+
+export const ButtonImage = styled.button<IButtonImageProps>`
+    height: 88px;
+    background: none;
+    border-radius: 20px;
+    overflow: hidden;
+    outline: none;
+    opacity: 0.6;
+
+    ${props => props.active && css`
+        opacity: 1;
+    `}
+
+    > img {
+        width: 100%;
+        height: 88px;
+        object-fit: cover;
+    }
+`
+
+export const OrphanageDetails = styled.div`
+    padding: 80px;
+
+    h1 {
+        color: #4D6F80;
+        font-family: Quicksand;
+        font-size: 54px;
+        line-height: 54px;
+        margin-bottom: 8px;
+    }
+
+    p {
+        color: #5C8599;
+        line-height: 28px;
+        margin-top: 24px;
+    }
+
+    hr {
+        width: 100%;
+        height: 1px;
+        border: 0;
+        background: #D3E2E6;
+        margin: 64px 0;
+    }
+
+    > h2 {
+        font-family: Quicksand;
+        font-size: 36px;
+        line-height: 46px;
+        color: #4D6F80;
+    }
+`
+
+export const MapContainer = styled.div`
+    margin-top: 64px;
+    background: #E6F7FB;
+    border: 1px solid #B3DAE2;
+    border-radius: 20px;
+
+    footer {
+        padding: 20px 0;
+        text-align: center;
+
+        a {
+        line-height: 24px;
+        color: #0089A5;
+        text-decoration: none;
+        }
+    }
+`
+
+export const OpenDetails = styled.div`
+    margin-top: 24px;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    column-gap: 20px;
+
+    div {
+        padding: 32px 24px;
+        border-radius: 20px;
+        line-height: 28px;
+        background: linear-gradient(149.97deg, #E6F7FB 8.13%, #FFFFFF 92.67%);
+        border: 1px solid #B3DAE2;
+        color: #5C8599;
+
+        > svg {
+            display: block;
+            margin-bottom: 20px;
+        }
+
+        & + div {
+            background: linear-gradient(149.97deg, #E6F7FB 8.13%, #FFFFFF 92.67%);
+            border: 1px solid #B3DAE2;
+            color: #5C8599;
+        }
+    }
+
+    > button {
+        margin-top: 64px;
+        width: 100%;
+        height: 64px;
+        background: #3CDC8C;
+        border-radius: 20px;
+        color: #FFFFFF;
+        font-weight: 800;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        transition: background-color 0.2s;
+
+        &:hover {
+            background: #36CF82;
+        }
+
+        svg {
+            margin-right: 16px;
+        }
+    }
+`
+// #page-orphanage aside {
+//   position: fixed;
+//   height: 100%;
+//   padding: 32px 24px;
+//   background: linear-gradient(329.54deg, #15B6D6 0%, #15D6D6 100%);
+
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: space-between;
+//   align-items: center;
+// }
+
+// #page-orphanage aside img {
+//   width: 48px;
+// }
+
+// #page-orphanage aside footer a,
+// #page-orphanage aside footer button {
+//   width: 48px;
+//   height: 48px;
+
+//   border: 0;
+
+//   background: #12AFCB;
+//   border-radius: 16px;
+
+//   cursor: pointer;
+
+//   transition: background-color 0.2s;
+
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+// }
+
+// #page-orphanage aside footer a:hover,
+// #page-orphanage aside footer button:hover {
+//   background: #17D6EB;
+// }
+
+// .orphanage-details .orphanage-details-content .map-container .leaflet-container {
+//   border-bottom: 1px solid #DDE3F0;
+//   border-radius: 20px;
+// }
+
+// .orphanage-details .orphanage-details-content .open-details div.open-on-weekends {
+//   background: linear-gradient(154.16deg, #EDFFF6 7.85%, #FFFFFF 91.03%);
+//   border: 1px solid #A1E9C5;
+//   color: #37C77F;
+// }
