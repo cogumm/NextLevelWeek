830a5b1a56fd734c49dc67b3651f320bd829da59
Gabriel F. Vilar <gabriel@cogumm.net>
2020-10-14 11:19:16 -0300

    #3 NLW Api - Novo ORMConfig com variáveis

---
+++ #3 NLW/backend/ormconfig.js
@@ -0,0 +1,32 @@
+// {
+//     "type": "sqlite",
+//     "database": "./src/database/database.sqlite",
+//     "migrations": [
+//         "./src/database/migrations/*.ts"
+//     ],
+//     "entities": [
+//         "./src/models/*.ts"
+//     ],
+//     "cli": {
+//         "migrationsDir": "./src/database/migrations"
+//     }
+// }
+const path = require('path');
+
+module.exports = {
+    type: process.env.TYPEORM_HOST || 'sqlite',
+    host: process.env.TYPEORM_HOST,
+    username: process.env.TYPEORM_USERNAME,
+    password: process.env.TYPEORM_PASSWORD,
+
+    database: './src/database/' + process.env.TYPEORM_DATABASE,
+    port: Number(process.env.TYPEORM_PORT),
+
+    migrations: [path.join(__dirname, 'src', 'database', 'migrations', '*.ts')],
+    entities: [path.join(__dirname, 'src', 'database', '*.ts')],
+    cli: {
+        migrationsDir: [path.join(__dirname, 'src', 'database', 'migrations')],
+    },
+
+    synchronize: process.env.TYPEORM_SYNCHRONIZE,
+};
