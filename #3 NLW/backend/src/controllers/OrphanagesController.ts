bb8f8b52cf0759038665b7e0527c1b59014b74f1
Gabriel F. Vilar <gabriel@cogumm.net>
2020-10-14 10:57:33 -0300

    #3 NLW Api - Cadastrando o boolean no open on weekends

--- #3 NLW/backend/src/controllers/OrphanagesController.ts
+++ #3 NLW/backend/src/controllers/OrphanagesController.ts
@@ -57,7 +57,7 @@ export default {
             about,
             instructions,
             opening_hours,
-            open_on_weekends,
+            open_on_weekends: JSON.parse(open_on_weekends),
             images
         };
 
