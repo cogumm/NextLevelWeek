b76caaf7a289fde45d31b5b1be943f88873a1351
Gabriel F. Vilar <gabriel@cogumm.net>
2020-10-15 17:36:45 -0300

    #3 NLW Mobile - Trabalhando com as rotas

---
+++ #3 NLW/mobile/src/routes.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const { Navigator, Screen } = createStackNavigator();
+
+import OrphanagesMap from './pages/OrphanagesMap';
+import Orphanage from './pages/Orphanage';
+
+export default function Routes() {
+    return (
+        <NavigationContainer>
+            <Navigator screenOptions={{ headerShown: false }}>
+                <Screen name="OrphanagesMap" component={OrphanagesMap} />
+                <Screen name="Orphanage" component={Orphanage} />
+            </Navigator>
+        </NavigationContainer>
+    );
+}
