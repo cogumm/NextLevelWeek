7420ccb55ad3b1b6dad2d016de3090e499f61069
Gabriel F. Vilar <gabriel@cogumm.net>
2021-02-23 19:17:05 -0300

    :lipstick: Componentized the ExperienceBar
    Create a Experiencebar.module.css

--- #4 NLW/ReactJS/moveit-next/src/components/ExperienceBar.tsx
+++ #4 NLW/ReactJS/moveit-next/src/components/ExperienceBar.tsx
@@ -1,11 +1,16 @@
+import styles from "../styles/components/ExperienceBar.module.css";
+
 export function ExperienceBar() {
     return (
-        <header className="experience-bar">
+        <header className={styles.experienceBar}>
             <span>0 xp</span>
             <div>
                 <div style={{ width: "50%" }} />
 
-                <span className="current-experience" style={{ left: "50%" }}>
+                <span
+                    className={styles.currentExperience}
+                    style={{ left: "50%" }}
+                >
                     300 xp
                 </span>
             </div>
