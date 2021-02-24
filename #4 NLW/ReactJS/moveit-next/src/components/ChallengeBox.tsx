06fc4e119e37b57626714095d6ee10b7bd3c4e78
Gabriel F. Vilar <gabriel@cogumm.net>
2021-02-24 18:48:37 -0300

    :lipstick: Finished ChallengeBox styles.

--- #4 NLW/ReactJS/moveit-next/src/components/ChallengeBox.tsx
+++ #4 NLW/ReactJS/moveit-next/src/components/ChallengeBox.tsx
@@ -1,15 +1,42 @@
 import styles from "../styles/components/ChallengeBox.module.css";
 
 export function ChallengeBox() {
+    const hasActiveChallenge = true;
+
     return (
         <div className={styles.challengeBoxContainer}>
-            <div className={styles.challengeNotActive}>
-                <strong>Finalize um ciclo para receber um desafio.</strong>
-                <p>
-                    <img src="icons/level-up.svg" alt="Level Up" />
-                    Avance de level completando desafios.
-                </p>
-            </div>
+            {hasActiveChallenge ? (
+                <div className={styles.challengeActive}>
+                    <header>Ganhe 400xp</header>
+                    <main>
+                        <img src="icons/body.svg" />
+                        <strong>Novo desafio</strong>
+                        <p>Faça alguma coisa!</p>
+                    </main>
+                    <footer>
+                        <button
+                            type="button"
+                            className={styles.challengeFailedButton}
+                        >
+                            Falhei
+                        </button>
+                        <button
+                            type="button"
+                            className={styles.challengeSucceededButton}
+                        >
+                            Completei
+                        </button>
+                    </footer>
+                </div>
+            ) : (
+                <div className={styles.challengeNotActive}>
+                    <strong>Finalize um ciclo para receber um desafio.</strong>
+                    <p>
+                        <img src="icons/level-up.svg" alt="Level Up" />
+                        Avance de level completando desafios.
+                    </p>
+                </div>
+            )}
         </div>
     );
 }
