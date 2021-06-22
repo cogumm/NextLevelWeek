334528282b0987ea41c7f39f95eaf9ecd8ef3908
Gabriel F. Vilar <cogumm@gmail.com>
2021-06-22 11:57:31 -0300

    NLW6 - Aula 02 - Botão como componente

---
+++ #6 NLW/letmeask/src/components/Button.tsx
@@ -0,0 +1,9 @@
+import { ButtonHTMLAttributes } from "react";
+
+import "../styles/button.scss";
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+
+export function Button(props: ButtonProps) {
+  return <button className="button" {...props} />;
+}
--- #6 NLW/letmeask/src/pages/Home.tsx
+++ #6 NLW/letmeask/src/pages/Home.tsx
@@ -2,6 +2,8 @@ import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
 import googleIconImg from "../assets/images/google-icon.svg";
 
+import { Button } from "../components/Button";
+
 import "../styles/auth.scss";
 
 export function Home() {
@@ -25,7 +27,7 @@ export function Home() {
           <div className="separator">ou entre em uma sala</div>
           <form>
             <input type="text" placeholder="Digite o código da sala" />
-            <button type="submit">Entrar na sala</button>
+            <Button type="submit">Entrar na sala</Button>
           </form>
         </div>
       </main>
