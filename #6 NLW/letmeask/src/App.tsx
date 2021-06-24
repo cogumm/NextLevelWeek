1ef7025079c9e8b5362a64c5b86a162c9d486ecb
Gabriel F. Vilar <cogumm@gmail.com>
2021-06-24 17:52:04 -0300

    NLW6 - Aula 04 - Página de sala (Admin)

--- #6 NLW/letmeask/src/App.tsx
+++ #6 NLW/letmeask/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import { AuthContextProvider } from "./contexts/AuthContext";
 
+import { AdminRoom } from "./pages/AdminRoom";
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 import { Room } from "./pages/Room";
@@ -14,6 +15,8 @@ function App() {
           <Route path="/" exact component={Home} />
           <Route path="/rooms/new" component={NewRoom} />
           <Route path="/rooms/:id" component={Room} />
+
+          <Route path="/admin/rooms/:id" component={AdminRoom} />
         </Switch>
       </AuthContextProvider>
     </BrowserRouter>
---
+++ #6 NLW/letmeask/src/pages/AdminRoom.tsx
@@ -0,0 +1,61 @@
+import { FormEvent, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import { useAuth } from "../hooks/useAuth";
+import { useRoom } from "../hooks/useRoom";
+
+import { database } from "../services/firebase";
+
+import { Button } from "../components/Button";
+import { RoomCode } from "../components/RoomCode";
+import { Question } from "../components/Question";
+
+import logoImg from "../assets/images/logo.svg";
+
+import "../styles/room.scss";
+
+type RoomParams = {
+  id: string;
+};
+
+export function AdminRoom() {
+  // "Pegando" o código da sala através dos parâmetros.
+  const params = useParams<RoomParams>();
+  const roomId = params.id;
+
+  // Importando o Hook useRoom.
+  const { questions, title } = useRoom(roomId);
+
+  return (
+    <div id="page-room">
+      <header>
+        <div className="content">
+          <img src={logoImg} alt="Letmeask" />
+          <div>
+            <RoomCode code={roomId} />
+            <Button isOutlined>Encerrar sala</Button>
+          </div>
+        </div>
+      </header>
+
+      <main>
+        <div className="room-title">
+          <h1>Sala {title}</h1>
+          {questions.length > 0 && <span>{questions.length} pergunta(s)</span>}
+        </div>
+
+        <div className="question-list">
+          {questions.map((question) => {
+            return (
+              <Question
+                key={question.id}
+                content={question.content}
+                author={question.author}
+              />
+            );
+          })}
+        </div>
+      </main>
+    </div>
+  );
+}
