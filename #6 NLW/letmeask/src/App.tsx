11c2f6c796202b4a3d1d61794b7e65353f23f9bb
Gabriel F. Vilar <cogumm@gmail.com>
2021-06-23 21:42:46 -0300

    NLW6 - Aula 03 - Fluxo de criação de sala.

--- #6 NLW/letmeask/src/App.tsx
+++ #6 NLW/letmeask/src/App.tsx
@@ -1,16 +1,20 @@
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import { AuthContextProvider } from "./contexts/AuthContext";
 
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
-
-import { AuthContextProvider } from "./contexts/AuthContext";
+import { Room } from "./pages/Room";
 
 function App() {
   return (
     <BrowserRouter>
       <AuthContextProvider>
-        <Route path="/" exact component={Home} />
-        <Route path="/rooms/new" component={NewRoom} />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/rooms/new" component={NewRoom} />
+          <Route path="/rooms/:id" component={Room} />
+        </Switch>
       </AuthContextProvider>
     </BrowserRouter>
   );
---
+++ #6 NLW/letmeask/src/pages/Room.tsx
@@ -0,0 +1,3 @@
+export function Room() {
+  return <h1>Sala</h1>;
+}
