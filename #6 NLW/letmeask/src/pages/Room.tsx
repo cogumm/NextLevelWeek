65c1e9c72c95c4a4e313f68d7d3c4d58de602157
Gabriel F. Vilar <cogumm@gmail.com>
2021-06-23 22:59:42 -0300

    NLW6 - Aula 03 - Página da sala (criação de pergunta)

--- #6 NLW/letmeask/src/pages/Room.tsx
+++ #6 NLW/letmeask/src/pages/Room.tsx
@@ -1,4 +1,8 @@
+import { FormEvent, useState } from "react";
 import { useParams } from "react-router-dom";
+
+import { useAuth } from "../hooks/useAuth";
+import { database } from "../services/firebase";
 
 import { Button } from "../components/Button";
 import { RoomCode } from "../components/RoomCode";
@@ -12,15 +16,55 @@ type RoomParams = {
 };
 
 export function Room() {
+  // Apenas usuários autenticados podem enviar novas perguntas.
+  const { user } = useAuth();
+
+  // Informação da nova pergunta.
+  const [newQuestion, setNewQuestion] = useState("");
+
   // "Pegando" o código da sala através dos parâmetros.
   const params = useParams<RoomParams>();
+  const roomId = params.id;
+
+  // Função para a criação de uma nova pergunta.
+  async function handleSendQuestion(event: FormEvent) {
+    event.preventDefault();
+
+    if (newQuestion.trim() === "") {
+      return;
+    }
+
+    // Se não existir nem um usuário, retornar um erro.
+    // Dica de Toast: https://react-hot-toast.com/
+    if (!user) {
+      throw new Error("You must be logged in.");
+    }
+
+    // Criando a pergunta em si.
+    const question = {
+      content: newQuestion,
+      author: {
+        name: user.name,
+        avatar: user.avatar,
+      },
+      // Se ela está com "Highlighte" o destaque que o Admin dá na pergunta.
+      isHighlighted: false,
+      // Se já foi respondida ou não.
+      isAnswered: false,
+    };
+
+    await database.ref(`rooms/${roomId}/questions`).push(question);
+
+    // "Apagando" a pergunta no textarea após ela ser enviada.
+    setNewQuestion("");
+  }
 
   return (
     <div id="page-room">
       <header>
         <div className="content">
           <img src={logoImg} alt="Letmeask" />
-          <RoomCode code={params.id} />
+          <RoomCode code={roomId} />
         </div>
       </header>
 
@@ -30,13 +74,26 @@ export function Room() {
           <span>5 perguntas</span>
         </div>
 
-        <form>
-          <textarea placeholder="O que deseja perguntar ?" />
+        <form onSubmit={handleSendQuestion}>
+          <textarea
+            placeholder="O que deseja perguntar ?"
+            onChange={(event) => setNewQuestion(event.target.value)}
+            value={newQuestion}
+          />
           <div className="form-footer">
-            <span>
-              Para enviar uma pergunta, <button>faça seu login</button>.
-            </span>
-            <Button type="submit">Enviar pergunta</Button>
+            {user ? (
+              <div className="user-info">
+                <img src={user.avatar} alt={user.name} />
+                <span>{user.name}</span>
+              </div>
+            ) : (
+              <span>
+                Para enviar uma pergunta, <button>faça seu login</button>.
+              </span>
+            )}
+            <Button type="submit" disabled={!user}>
+              Enviar pergunta
+            </Button>
           </div>
         </form>
       </main>
