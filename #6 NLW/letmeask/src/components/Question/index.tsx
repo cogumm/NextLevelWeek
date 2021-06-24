94d4f2f40cc73962fada4aab94482ac7d2801853
Gabriel F. Vilar <cogumm@gmail.com>
2021-06-24 17:32:11 -0300

    NLW6 - Aula 04 - Estrutura das perguntas

---
+++ #6 NLW/letmeask/src/components/Question/index.tsx
@@ -0,0 +1,24 @@
+import "./styles.scss";
+
+type QuestionProps = {
+  content: string;
+  author: {
+    name: string;
+    avatar: string;
+  };
+};
+
+export function Question({ content, author }: QuestionProps) {
+  return (
+    <div className="question">
+      <p>{content}</p>
+      <footer>
+        <div className="user-info">
+          <img src={author.avatar} alt={author.name} />
+          <span>{author.name}</span>
+        </div>
+        <div></div>
+      </footer>
+    </div>
+  );
+}
--- #6 NLW/letmeask/src/pages/Room.tsx
+++ #6 NLW/letmeask/src/pages/Room.tsx
@@ -6,6 +6,7 @@ import { database } from "../services/firebase";
 
 import { Button } from "../components/Button";
 import { RoomCode } from "../components/RoomCode";
+import { Question } from "../components/Question";
 
 import logoImg from "../assets/images/logo.svg";
 
@@ -24,7 +25,7 @@ type FirebaseQuestions = Record<
   }
 >;
 
-type Question = {
+type QuestionType = {
   id: string;
   author: {
     name: string;
@@ -45,7 +46,7 @@ export function Room() {
 
   // Informação da nova pergunta.
   const [newQuestion, setNewQuestion] = useState("");
-  const [questions, setQuestions] = useState<Question[]>([]);
+  const [questions, setQuestions] = useState<QuestionType[]>([]);
   const [title, setTitle] = useState("");
 
   // "Pegando" o código da sala através dos parâmetros.
@@ -154,7 +155,18 @@ export function Room() {
           </div>
         </form>
 
-        {JSON.stringify(questions)}
+        {/* {JSON.stringify(questions)} */}
+        <div className="question-list">
+          {questions.map((question) => {
+            return (
+              <Question
+                key={question.id}
+                content={question.content}
+                author={question.author}
+              />
+            );
+          })}
+        </div>
       </main>
     </div>
   );
