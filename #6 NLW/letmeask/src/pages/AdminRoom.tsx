3e22a0f3998d7aa462c42d49864f9a226e93e3b1
Gabriel F. Vilar <cogumm@gmail.com>
2021-06-25 20:07:13 -0300

    NLW6 - Aula 04 - Funcionalidade de remover a pergunta

--- #6 NLW/letmeask/src/pages/AdminRoom.tsx
+++ #6 NLW/letmeask/src/pages/AdminRoom.tsx
@@ -3,11 +3,14 @@ import { useParams } from "react-router-dom";
 // import { useAuth } from "../hooks/useAuth";
 import { useRoom } from "../hooks/useRoom";
 
+import { database } from "../services/firebase";
+
 import { Button } from "../components/Button";
 import { RoomCode } from "../components/RoomCode";
 import { Question } from "../components/Question";
 
 import logoImg from "../assets/images/logo.svg";
+import deleteImg from "../assets/images/delete.svg";
 
 import "../styles/room.scss";
 
@@ -25,6 +28,13 @@ export function AdminRoom() {
 
   // Importando o Hook useRoom.
   const { questions, title } = useRoom(roomId);
+
+  // Função para remover a pergunta.
+  async function handleDeleteQuestion(questionId: string) {
+    if (window.confirm("Você tem certeza que deseja excluir esta pergunta ?")) {
+      await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
+    }
+  }
 
   return (
     <div id="page-room">
@@ -51,7 +61,14 @@ export function AdminRoom() {
                 key={question.id}
                 content={question.content}
                 author={question.author}
-              />
+              >
+                <button
+                  type="button"
+                  onClick={() => handleDeleteQuestion(question.id)}
+                >
+                  <img src={deleteImg} alt="Remover pergunta" />
+                </button>
+              </Question>
             );
           })}
         </div>
