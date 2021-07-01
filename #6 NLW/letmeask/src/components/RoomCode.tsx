c7380a6b6d29060e25589887ee55076a7f92d3ac
Gabriel F. Vilar <cogumm@gmail.com>
2021-07-01 00:47:05 -0300

    NLW6 - Versão mobile Room e RoomCode com Toast

--- #6 NLW/letmeask/src/components/RoomCode.tsx
+++ #6 NLW/letmeask/src/components/RoomCode.tsx
@@ -1,3 +1,5 @@
+import { Toaster, toast } from "react-hot-toast";
+
 import copyImg from "../assets/images/copy.svg";
 
 import "../styles/room-code.scss";
@@ -9,15 +11,25 @@ type RoomCodeProps = {
 export function RoomCode(props: RoomCodeProps) {
   // Função de copar o código da sala.
   function copyRoomCodeToClipboard() {
+    toast.success("Código da sala copiado com sucesso!", {
+      id: props.code,
+      style: {
+        width: "40rem",
+      },
+    });
+
     navigator.clipboard.writeText(props.code);
   }
 
   return (
-    <button className="room-code" onClick={copyRoomCodeToClipboard}>
-      <div>
-        <img src={copyImg} alt="Copy room code" />
-      </div>
-      <span>Sala #{props.code}</span>
-    </button>
+    <>
+      <Toaster position="top-right" reverseOrder={false} />
+      <button className="room-code" onClick={copyRoomCodeToClipboard}>
+        <div>
+          <img src={copyImg} alt="Copy room code" />
+        </div>
+        <span>Sala #{props.code}</span>
+      </button>
+    </>
   );
 }
